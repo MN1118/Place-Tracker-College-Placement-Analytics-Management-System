dc689cd88522329aef2e36698be6ec80044bb840
MN1118 <nimbalkarmohit69@gmail.com>
2026-09-24 06:23:53 +0530

    new change

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 const { PORT, NODE_ENV } = require('./config/env');
 
-app.listen(PORT, () => {
-  console.log(`Placement System API running in ${NODE_ENV} mode on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(
+    `Placement System API running in ${NODE_ENV} mode on port ${PORT}`
+  );
 });
